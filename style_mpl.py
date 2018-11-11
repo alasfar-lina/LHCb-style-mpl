4ce3967fafbaa28240b559fd55ea01aa0155510f
Lina Alasfar <lina.alasfar@cern.ch>
2018-11-11 09:47:55 +0100

    more features

--- style_mpl.py
+++ style_mpl.py
@@ -1,6 +1,8 @@
 """
 LHCb-like style for matplotlib
 """
+
+
 __all__ = [
     'style_mpl',
 ]
@@ -9,39 +11,44 @@ def style_mpl():
 
     STYLE = {}
 
-    STYLE['lines.linewidth'] = 2
+    STYLE['lines.linewidth'] = 1.5
 
     # font
-    STYLE['font.family'] = "Times New Roman"
+    STYLE['text.usetex'] = True
+    STYLE['font.family'] = 'serif'
     STYLE['mathtext.fontset'] = 'stix'
-    STYLE['mathtext.default'] = 'rm'
+   # STYLE['mathtext.default'] = 'rm'
     # helvetica usually not present on linux
-
-
+    STYLE['font.serif'] ='Times New Roman'
     # figure layout
-    STYLE['figure.figsize'] = 8.75, 5.92
+    STYLE['figure.figsize'] =6,7
     #   lhcbStyle->SetPaperSize(20,26); # in cm
     # STYLE['figure.figsize'] =  10.2362205, 7.874015 # in inc, not working
     STYLE['figure.facecolor'] = 'white'
-    STYLE['figure.subplot.bottom'] = 0.16
+    STYLE['figure.subplot.bottom'] = 0.14
     STYLE['figure.subplot.top'] = 0.95
     STYLE['figure.subplot.left'] = 0.14
     STYLE['figure.subplot.right'] = 0.95
-
+    STYLE['savefig.facecolor']= 'ff000000'
     # axes
-    STYLE['axes.labelsize'] = 20
-    STYLE['xtick.labelsize'] = 19
-    STYLE['xtick.major.size'] = 12
-    STYLE['xtick.minor.size'] = 6
-    STYLE['ytick.labelsize'] = 10
-    STYLE['ytick.major.size'] = 14
-    STYLE['ytick.minor.size'] = 7
-    STYLE['lines.markersize'] = 8
+    STYLE['axes.xmargin']= 0
+    STYLE['axes.ymargin']= 0.05
+    STYLE['axes.labelsize'] = 22
+    STYLE['xtick.labelsize'] =12
+    STYLE['xtick.major.size'] = 7.5
+    STYLE['xtick.minor.size'] = 3.75
+    STYLE['ytick.labelsize'] = 12
+    STYLE['ytick.major.size'] = 7.5
+    STYLE['ytick.minor.size'] = 3.75
+    STYLE['lines.markersize'] = 5
+    STYLE['xtick.minor.bottom'] =True
+    STYLE['xtick.top'] =True
+    STYLE['ytick.right'] =True
     # STYLE['lines.markeredgewidth'] = 0. # not working, it changes other stuff
 
     # legend
     STYLE['legend.numpoints'] = 1
-    STYLE['legend.fontsize'] = 19
+    STYLE['legend.fontsize'] = 13
     STYLE['legend.labelspacing'] = 0.3
     STYLE['legend.frameon'] = False
 
@@ -51,4 +58,4 @@ def style_mpl():
     # * axis-label offset
     # * axis-ticks
 
-return STYLE
+    return STYLE
